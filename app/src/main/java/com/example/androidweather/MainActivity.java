4190deb207a5001940b52d53620f5741cfb25db2
Hugh <hcl212@users.noreply.github.com>
2019-09-26 11:09:08 -0400

    summary done

--- app/src/main/java/com/example/androidweather/MainActivity.java
+++ app/src/main/java/com/example/androidweather/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.example.androidweather.model.Example;
 import com.example.androidweather.services.RetrofitInstance;
@@ -16,21 +17,29 @@ import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
+    // TODO: store api key private
+    // TODO: get user lat and long from GPS
     private Double currTemp = 0.0;
     private Double latitude = 40.712776;
     private Double longitude = -74.005974;
     private String key = "2daafb9d2b0e6083c298882e99e100aa";
+
+    private TextView tempView;
+    private TextView weatherStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tempView = findViewById(R.id.temp);
+        weatherStatus = findViewById(R.id.status);
+
         getForecast();
     }
 
     public Object getForecast() {
-        WeatherService weatherService = RetrofitInstance.getWeatherService();
+        final WeatherService weatherService = RetrofitInstance.getWeatherService();
         Call<Example> call = weatherService.getWeatherData(key, latitude, longitude);
 
         call.enqueue(new Callback<Example>() {
@@ -40,6 +49,10 @@ public class MainActivity extends AppCompatActivity {
 
                 if (forecast != null && forecast.getCurrently() != null){
                     currTemp = forecast.getCurrently().getTemperature();
+                    String tempString = Double.toString(currTemp);
+                    tempView.setText(tempString);
+
+                    weatherStatus.setText(forecast.getCurrently().getSummary());
                 }
             }
 
