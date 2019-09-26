b8578a9700e7e3e7c1ff0171fb7515378c26ffa8
Hugh <hcl212@users.noreply.github.com>
2019-09-26 09:07:59 -0400

    json to pojo

---
+++ app/src/main/java/com/example/androidweather/services/RetrofitInstance.java
@@ -0,0 +1,21 @@
+package com.example.androidweather.services;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+
+    private static Retrofit retrofit = null;
+    private static String BASE_URL = "https://api.darksky.net/forecast/";
+
+    public static GetWeatherService getWeatherService() {
+
+        if (retrofit== null) {
+            retrofit = new Retrofit
+                    .Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+    }
+}
